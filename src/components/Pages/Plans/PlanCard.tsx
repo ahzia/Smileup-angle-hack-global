d7f090417d0f6da131afc62c0526bba201f3fd9e
ahzia <ah.ziayosfi@gmail.com>
2024-10-21 23:35:04 +0200

    added some design to the plans card component

--- src/components/Pages/Plans/PlanCard.tsx
+++ src/components/Pages/Plans/PlanCard.tsx
@@ -1,5 +1,6 @@
 import { Plan } from "@/models/Plan";
 import Image from "next/image";
+import { FiMapPin, FiSmile, FiUsers, FiDollarSign } from "react-icons/fi"; // Importing relevant icons
 
 interface PlanCardProps {
   plan: Plan;
@@ -7,23 +8,53 @@ interface PlanCardProps {
 
 export default function PlanCard({ plan }: PlanCardProps) {
   return (
-    <div className="bg-[#282C34] p-4 rounded-lg shadow-md space-y-2">
-      <Image src={plan.image} alt={plan.name} />
-      <h3 className="text-xl font-bold">{plan.name}</h3>
-      <p className="text-sm text-gray-600">Location: {plan.location}</p>
-      <p
-        className={`text-sm ${
-          plan.smiles >= 0 ? "text-green-500" : "text-red-500"
-        }`}
-      >
-        Smiles: {plan.smiles}
-      </p>
-      <p className="text-sm text-gray-600">
-        Participants: {plan.participants}/{plan.max_participants}
-      </p>
-      <p className="text-sm text-gray-600">
-        {plan.is_free ? "Join for Free" : "Paid Event"}
-      </p>
+    <div className="bg-[#282C34] p-4 rounded-lg shadow-md space-y-2 h-full flex flex-col justify-between">
+      <Image
+        src={plan.image}
+        alt={plan.name}
+        className="w-full h-32 object-cover rounded-lg"
+      />
+      <h3 className="text-xl font-bold text-white">{plan.name}</h3>
+      <div className="flex flex-col space-y-2 text-gray-400">
+        <div className="flex items-center space-x-1">
+          <FiMapPin className="text-yellow-400" />
+          <p className="text-sm">{plan.location}</p>
+        </div>
+        <div className="flex items-center space-x-1">
+          <FiSmile
+            className={`${
+              plan.smiles >= 0 ? "text-green-500" : "text-red-500"
+            }`}
+          />
+          <p className="text-sm">
+            Smiles:{" "}
+            <span
+              className={plan.smiles >= 0 ? "text-green-500" : "text-red-500"}
+            >
+              {plan.smiles}
+            </span>
+          </p>
+        </div>
+        <div className="flex items-center space-x-1">
+          <FiUsers className="text-blue-400" />
+          <p className="text-sm">
+            Participants: {plan.participants}/{plan.max_participants}
+          </p>
+        </div>
+        <div className="flex items-center space-x-1">
+          {plan.is_free ? (
+            <>
+              <FiDollarSign className="text-green-500" />
+              <p className="text-sm text-green-500">Join for Free</p>
+            </>
+          ) : (
+            <>
+              <FiDollarSign className="text-yellow-500" />
+              <p className="text-sm text-yellow-500">Paid Event</p>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
