daec4a2a8527b2f770cf813571f0d450bbf9b7b0
ahzia <ah.ziayosfi@gmail.com>
2024-10-24 16:50:15 +0430

    update color configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,15 +11,20 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        mainBg: '#130828',      // Main Background
-        secondary: '#262450',   // Buttons and Nav Background
-        cardBg: '#232627',      // Grey for Cards Background
-        textPrimary: '#FFFFFF', // Primary Text Color
-        textSecondary: '#ACADB9', // Secondary Text Color
-        highlight: '#00D7FF',   // Active/Helper Color
+        mainBg: '#1A1034',       // Slightly lighter Main Background for easier readability
+        secondary: '#2E2A5A',    // Nav Background can be used for buttons background
+        cardBg: '#2B2F33',       // Cards Background
+        cardBgLight: '#242830',  // Lighter shade for Cards Background
+        textPrimary: '#F5F5F5',  // Primary Text
+        textSecondary: '#B3B4C1', // Warmer grey for Secondary Text
+        highlight: '#FF6900',     // Active/Helper Color, lyf branding color this can aso be used for buttons
+        success: '#4CAF50',      // Success state color (green)
+        error: '#F44336',        // Error state color (red)
+        warning: '#FF9800',      // Warning state color (orange)
       },
     },
   },
   plugins: [],
 };
+
 export default config;
