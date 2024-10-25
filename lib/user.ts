7e0dfb4120a92ce1bb5d528cceefc94f7b09a693
Pushkar Sareen <sareenpushkar46@gmail.com>
2024-10-25 23:19:59 +0200

    image data updated

--- lib/user.ts
+++ lib/user.ts
@@ -1,4 +1,7 @@
 import ahmadPic from "../images/Users/Ahmadzia.png";
+import tamimPic from "../images/Users/tamim.jpeg";
+import pushkarPic from "../images/Users/pushkar.jpg";
+import fistonPic from "../images/Users/fiston.jpeg"
 
 
 export const users = [
@@ -18,7 +21,28 @@ export const users = [
     name: "Bob Smith",
     age: 30,
     email: "bob.smith@example.com",
-    profilePic: ahmadPic,
+    profilePic: pushkarPic,
+    interests: ["Cycling", "Programming", "Movies"],
+    residency: "lyf Funan Singapore",
+    smiles: 38,
+    bio: "Tech geek who loves cycling and binge-watching movies on weekends."
+  },
+  {
+    id: 3,
+    name: "john willson",
+    age: 33,
+    email: "bob.smith@example.com",
+    profilePic: tamimPic,
+    interests: ["Cycling", "Programming", "Movies"],
+    residency: "lyf Funan Singapore",
+    smiles: 38,
+    bio: "Tech geek who loves cycling and binge-watching movies on weekends."
+  },  {
+    id: 4,
+    name: "jayse joseph",
+    age: 28,
+    email: "bob.smith@example.com",
+    profilePic: fistonPic,
     interests: ["Cycling", "Programming", "Movies"],
     residency: "lyf Funan Singapore",
     smiles: 38,
@@ -26,11 +50,12 @@ export const users = [
   }
 ];
 
+
 export const userData = {
   name: "John Doe",
   age: 30,
   email: "example@gmail.com",
-  profilePic: ahmadPic,
+  profilePic: tamimPic,
   smiles: 100,
   bio: 'Digital Nomad | Travel Enthusiast'
 };
